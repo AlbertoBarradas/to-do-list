1fff239a333c6bb97ba95e4015f8d8797251046f
AlbertoBarradas <sebastianbago@hotmail.com>
2025-10-01 22:55:17 -0600

    feature: views created

--- todo_list/base/models.py
+++ todo_list/base/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200, null=True, blank=True)
-    description =
-    complete = 
-    create =
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['complete']
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import ListView
+from .models import Task
 
 
 # Create your views here.
+class TaskList(ListView):
+    model = Task
