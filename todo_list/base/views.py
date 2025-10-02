81ceca5cc46ab5b850104aec027398ec53dd8b8c
AlbertoBarradas <sebastianbago@hotmail.com>
2025-10-02 16:40:47 -0600

    feat: login-and-logout-views

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
 from .models import Task
 from django.urls import reverse_lazy
+
+class CustomLoginView(LoginView):
+    template_name = "base/login.html"
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+    
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 
 class TaskList(ListView):
     model = Task
