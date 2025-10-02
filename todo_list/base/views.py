1553c5afe5bd2fe64879f4b8106e806668265379
AlbertoBarradas <sebastianbago@hotmail.com>
2025-10-02 00:17:01 -0600

    feature: CRUD actions completed

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Task
+from django.urls import reverse_lazy
 
-
-# Create your views here.
 class TaskList(ListView):
     model = Task
+    context_object_name = "tasks"
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+    template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy("tasks")
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+    
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = "task"
+    success_url = reverse_lazy('tasks')
